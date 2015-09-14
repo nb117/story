6afc49b2372d21cfbad4d3300da53105f6a4dc1f
Noah Burbage <noah_burbage@yahoo.com>
2015-09-14 19:00:51 -0400

    save

--- story/intro.py
+++ story/intro.py
@@ -1,5 +1,9 @@
 """Introduction
 
 You open your eyes after a long slumber. Awaykend, you see the hot sun
-in the sky as clouds pass by it. you begin to stand on your
+in the sky as clouds pass by it. you begin to stand on your feet, looking around you, you see large feilds as long as the eye can see.
 """
+prompt('Where am I? And how did i get here?')
+on_I should get going='walking'
+
+
