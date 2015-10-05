4e8cb8c880b7f431124795ced9406e45206a5138
Noah Burbage <noah_burbage@yahoo.com>
2015-10-05 18:59:15 -0400

    save

---
+++ story/question.py
@@ -0,0 +1,4 @@
+"""question
+
+"You have to help me! the hive are behind me and if they find me their gonna kill me!!" you ask the creature why the want to kill him. "well, lets just say i just save someone's butt from being the hive's brunch." he points to a robot under a tree it looks beat up.
+
--- story/walking.py
+++ story/walking.py
@@ -1,7 +1,9 @@
 """Introductions
 
-as you begin to move your legs, you hear the sound of someone's foot steps. the sound gets closer and closer, until it becomes disturbing! then you look behind you back
-and see a humaniod like creature running really fast and its coming right towards you! "GET OUT OF THE WAY!!!!" the creature yells. as you jump to the side,
-it juumps into a bush and hides. you walk towards the creature and ask it a question.
+as you begin to move your legs, you hear the sound of someone's foot steps. the sound gets closer and closer, 
+until it becomes disturbing! then you look behind your back and see a humaniod like creature running really fast and its coming right towards you! "GET OUT OF THE WAY!!!!" the creature yells. 
+as you jump to the side,
+it jumps into a bush and hides. you walk towards the creature and ask it a question.
 """
-
+prompt = 'hey! whats wrong?'
+on_g = 'question'
